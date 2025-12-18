8c4856b9f07ef2491a7bed56c5710c0f245dc79f
Milouhaj <101606388+Milouhaj@users.noreply.github.com>
2025-12-18 12:55:22 +0100

    Add video metadata array to Player1.js

--- Player1.js
+++ Player1.js
@@ -1,1 +1,12 @@
-alert("player.js est bien chargé");
+[
+  {
+    "title": "🎥 Test MP4 – Divine Living",
+    "url": "https://www.divineliving.com/videos/talkshow/season1/wednesday/Wednesday_ep1.mp4"
+  },
+  {
+    "title": "🎥 Test MP4 – Archive.org",
+    "url": "https://archive.org/download/Forever.S01E01.HDTV.MyEgy.To.BySmSma/Forever.S01E01.HDTV.MyEgy.To.BySmSma.mp4"
+  }
+]
+
+
